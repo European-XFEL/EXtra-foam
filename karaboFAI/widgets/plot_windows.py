94fac279b6fed36b24fa1840570eb8dd72f7cffc
zhujun <zhujun981661@gmail.com>
2018-12-10 15:01:57 +0100

    DOC: document LaserOnOffWindow

--- karaboFAI/widgets/plot_windows.py
+++ karaboFAI/widgets/plot_windows.py
@@ -432,13 +432,15 @@ class IndividualPulseWindow(PlotWindow):
 class LaserOnOffWindow(PlotWindow):
     """LaserOnOffWindow class.
 
-    A window which visualizes the moving average of the average of the
-    azimuthal integration of all laser-on and laser-off pulses, as well
-    as their difference, in the upper plot. It also visualize the
-    evolution of the figure of merit (FOM), which is integration of the
-    absolute difference between the moving average of the laser-on and
-    laser-off results, for each pair of laser-on and laser-off trains,
-    in the lower plot.
+    The `optical laser pattern window` is used in pump-and-probe
+    experiments with an optical laser. It monitors:
+
+    - The moving average of the average of the azimuthal integration
+      of all laser-on and laser-off pulses, as well as their difference;
+    - The evolution of the figure of merit (FOM), which is integration
+      of the absolute difference between the moving average of the
+      laser-on and laser-off results, for each pair of laser-on and
+      laser-off trains.
     """
     plot_w = 800
     plot_h = 320
