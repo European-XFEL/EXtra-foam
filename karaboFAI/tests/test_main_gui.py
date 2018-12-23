ee0d2d0fdc70d4a57697a572169ce3e00323831f
zhujun98 <zhujun981661@gmail.com>
2018-12-23 18:52:21 +0100

    TEST: add test close windows for MainGUI

--- karaboFAI/tests/test_main_gui.py
+++ karaboFAI/tests/test_main_gui.py
@@ -13,7 +13,7 @@ class TestMainGui(unittest.TestCase):
     def testInstantiateOverviewWindow(self):
         pass
 
-    def testOpenWindows(self):
+    def testOpenCloseWindows(self):
         count = 0
         for idx in (2, 3, 4):
             count += 1
@@ -22,3 +22,10 @@ class TestMainGui(unittest.TestCase):
 
         self.actions[5].trigger()
         self.assertEqual(count, len(self.gui._plot_windows))
+
+        with self.assertRaises(StopIteration):
+            i = 0
+            while i < 100 and self.gui._plot_windows.keys():
+                key = next(self.gui._plot_windows.keys())
+                key.close()
+                i += 1
