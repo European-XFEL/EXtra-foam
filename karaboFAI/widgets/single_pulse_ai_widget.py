418df59c8ad9350b1eeaa73ff682619427ee2376
zhujun <zhujun981661@gmail.com>
2019-01-29 16:26:11 +0100

    Set plot_mean a config in SinglePulseAiWidget.
    Use SinglePulseAiWidget in OverviewWindowTrainResolved.

--- karaboFAI/widgets/single_pulse_ai_widget.py
+++ karaboFAI/widgets/single_pulse_ai_widget.py
@@ -21,18 +21,26 @@ class SinglePulseAiWidget(PlotWidget):
     result of individual pulses. The azimuthal integration result is also
     compared with the average azimuthal integration of all the pulses.
     """
-    def __init__(self, *, parent=None):
-        """Initialization."""
+    def __init__(self, *, plot_mean=True, parent=None):
+        """Initialization.
+
+        :param bool plot_mean: whether to plot the mean AI of all pulses
+            if the data is pulse resolved.
+        """
         super().__init__(parent=parent)
 
         self.pulse_id = 0
 
         self.setLabel('left', "Scattering signal (arb. u.)")
         self.setLabel('bottom', "Momentum transfer (1/A)")
-        self.addLegend(offset=(-40, 20))
 
         self._pulse_plot = self.plot(name="pulse_plot", pen=PenFactory.yellow)
-        self._mean_plot = self.plot(name="mean", pen=PenFactory.cyan)
+
+        if plot_mean:
+            self._mean_plot = self.plot(name="mean", pen=PenFactory.cyan)
+            self.addLegend(offset=(-40, 20))
+        else:
+            self._mean_plot = None
 
     def clear(self):
         """Override."""
@@ -41,14 +49,20 @@ class SinglePulseAiWidget(PlotWidget):
     def reset(self):
         """Override."""
         self._pulse_plot.setData([], [])
-        self._mean_plot.setData([], [])
+        if self._mean_plot is not None:
+            self._mean_plot.setData([], [])
 
     def update(self, data):
         """Override."""
-        try:
-            self._pulse_plot.setData(data.momentum, data.intensity[self.pulse_id])
-        except IndexError as e:
-            logger.error("<VIP pulse ID 1/2>: " + str(e))
-            return
+        if data.intensity.ndim == 3:
+            try:
+                self._pulse_plot.setData(data.momentum,
+                                         data.intensity[self.pulse_id])
+            except IndexError as e:
+                logger.error("<VIP pulse ID 1/2>: " + str(e))
+                return
+        else:
+            self._pulse_plot.setData(data.momentum, data.intensity)
 
-        self._mean_plot.setData(data.momentum, data.intensity_mean)
+        if self._mean_plot is not None:
+            self._mean_plot.setData(data.momentum, data.intensity_mean)
