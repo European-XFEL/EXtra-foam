771f812e481e6a3f07ce3bcc0e6d7c35f4acfdd7
zhujun <zhujun981661@gmail.com>
2019-02-01 08:39:17 +0100

    Preliminary implementation of ROICtrlWidget and MaskCtrlWidget
    In ImageToolWindow

--- karaboFAI/windows/image_tool_window.py
+++ karaboFAI/windows/image_tool_window.py
@@ -16,6 +16,58 @@ from .base_window import AbstractWindow, SingletonWindow
 from ..logger import logger
 
 
+class ROICtrlWidget(QtGui.QGroupBox):
+
+    def __init__(self, title, *, parent=None):
+        """"""
+        super().__init__(title, parent=parent)
+
+        self._roi_width_le = QtGui.QLineEdit()
+        self._roi_height_le = QtGui.QLineEdit()
+
+        self._lock_cb = QtGui.QCheckBox("Lock")
+        self._activate_cb = QtGui.QCheckBox("Activate")
+
+        self.initUI()
+
+    def initUI(self):
+        wh_layout = QtGui.QHBoxLayout()
+        wh_layout.addWidget(QtGui.QLabel("Width: "))
+        wh_layout.addWidget(self._roi_width_le)
+        wh_layout.addWidget(QtGui.QLabel("Height: "))
+        wh_layout.addWidget(self._roi_height_le)
+
+        cb_layout = QtGui.QHBoxLayout()
+        cb_layout.addWidget(self._activate_cb)
+        cb_layout.addWidget(self._lock_cb)
+
+        layout = QtGui.QVBoxLayout()
+
+        layout.addLayout(cb_layout)
+        layout.addLayout(wh_layout)
+
+        self.setLayout(layout)
+
+
+class MaskCtrlWidget(QtGui.QGroupBox):
+
+    def __init__(self, title, *, parent=None):
+        """"""
+        super().__init__(title, parent=parent)
+
+        self._lock_cb = QtGui.QCheckBox("Lock")
+
+        self.initUI()
+
+    def initUI(self):
+
+        layout = QtGui.QVBoxLayout()
+
+        layout.addWidget(self._lock_cb)
+
+        self.setLayout(layout)
+
+
 @SingletonWindow
 class ImageToolWindow(AbstractWindow):
     """ImageToolWindow class."""
@@ -25,32 +77,33 @@ class ImageToolWindow(AbstractWindow):
         super().__init__(data, parent=parent)
 
         self._image_view = ImageView()
+        self._roi_ctrl1 = ROICtrlWidget("ROI 1")
+        self._roi_ctrl2 = ROICtrlWidget("ROI 2")
+        self._mask_panel = MaskCtrlWidget("Mask tool")
 
         self._update_image_btn = QtGui.QPushButton("Update image")
         self._update_image_btn.clicked.connect(self.updateImage)
 
         self.initUI()
-
+        self.resize(800, 600)
         self.updateImage()
 
         logger.info("Open DrawMaskWindow")
 
     def initUI(self):
         """Override."""
-        self.initPlotUI()
+        tool_layout = QtGui.QGridLayout()
+        tool_layout.addWidget(self._roi_ctrl1, 0, 0, 1, 1)
+        tool_layout.addWidget(self._roi_ctrl2, 1, 0, 1, 1)
+        tool_layout.addWidget(self._mask_panel, 0, 1, 2, 1)
 
-        layout = QtGui.QHBoxLayout()
+        layout = QtGui.QVBoxLayout()
         layout.addWidget(self._image_view)
         layout.addWidget(self._update_image_btn)
+        layout.addLayout(tool_layout)
+
         self._cw.setLayout(layout)
-
-    def initPlotUI(self):
-        """Override."""
-        pass
-
-    def initCtrlUI(self):
-        """Override."""
-        pass
+        self._cw.layout().setContentsMargins(0, 0, 0, 0)
 
     def updateImage(self):
         """For updating image manually."""
