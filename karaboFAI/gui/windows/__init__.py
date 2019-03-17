78a98248241fbc461af44976e3f571fa99e43374
zhujun98 <zhujun981661@gmail.com>
2019-03-17 13:08:02 +0100

    Remove draw_mask.py

--- karaboFAI/gui/windows/__init__.py
+++ karaboFAI/gui/windows/__init__.py
@@ -1,6 +1,5 @@
 from .bragg_spot import BraggSpotsWindow
 from .correlation import CorrelationWindow
-from .draw_mask import DrawMaskWindow
 from .image_tool import ImageToolWindow
 from .overview import OverviewWindow
 
@@ -8,7 +7,6 @@ from .overview import OverviewWindow
 __all__ = [
     "BraggSpotsWindow",
     "CorrelationWindow",
-    "DrawMaskWindow",
     "ImageToolWindow",
     "OverviewWindow"
 ]
