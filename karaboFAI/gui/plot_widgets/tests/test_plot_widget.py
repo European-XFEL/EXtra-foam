1257968a9c5b058b5579e18078dfdd2043b0af63
zhujun <zhujun981661@gmail.com>
2019-04-02 12:23:19 +0200

    TEST: extend unittest for PlotWidget

--- karaboFAI/gui/plot_widgets/tests/test_plot_widget.py
+++ karaboFAI/gui/plot_widgets/tests/test_plot_widget.py
@@ -20,17 +20,36 @@ class TestPlotWidget(unittest.TestCase):
         self._widget.clear()
         self.assertFalse(self._widget.plotItem.items)
 
-    def testErrorBarPlot(self):
-        plot = self._widget.plotErrorBar()
+    def testBarPlot(self):
+        # set any valid number
+        plot = self._widget.plotBar([1, 2], [3, 4])
+        app.processEvents()
 
+        # test set empty data
         plot.setData([], [])
         app.processEvents()
 
+        # test if x and y have different lengths
+        with self.assertRaises(ValueError):
+            plot.setData([1, 2, 3], [])
+
+    def testErrorBarPlot(self):
+        # set any valid number
+        plot = self._widget.plotErrorBar([1, 2], [3, 4])
+        app.processEvents()
+
+        # set x, y, y_min and y_max together
+        plot.setData([1, 2], [1, 2], y_min=[0, 0], y_max=[2, 2])
+        app.processEvents()
+
+        # test set empty data
+        plot.setData([], [])
+        app.processEvents()
+
+        # test if x and y have different lengths
         with self.assertRaises(ValueError):
             plot.setData([1, 2, 3], [], y_min=[0, 0, 0], y_max=[2, 2, 2])
 
+        # test if y_min/ymax have different lengths
         with self.assertRaises(ValueError):
             plot.setData([1, 2, 3], [1, 2, 3], y_min=[0, 0, 0], y_max=[2, 2])
-
-        plot.setData([1, 2], [1, 2], y_min=[0, 0], y_max=[2, 2])
-        app.processEvents()
