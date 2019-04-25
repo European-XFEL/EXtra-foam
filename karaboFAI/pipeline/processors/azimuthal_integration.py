d8cebbc1d9b37b88a0e5b709f20038e9a042827c
zhujun98 <zhujun981661@gmail.com>
2019-04-25 19:55:29 +0200

    Comment

--- karaboFAI/pipeline/processors/azimuthal_integration.py
+++ karaboFAI/pipeline/processors/azimuthal_integration.py
@@ -3,7 +3,7 @@ Offline and online data analysis and visualization tool for azimuthal
 integration of different data acquired with various detectors at
 European XFEL.
 
-Data processors.
+AzimuthalIntegrationProcessor.
 
 Author: Jun Zhu <jun.zhu@xfel.eu>
 Copyright (C) European X-Ray Free-Electron Laser Facility GmbH.
