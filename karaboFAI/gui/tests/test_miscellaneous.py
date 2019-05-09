e23b918157d2906ba8f24d81f73e92681fe66e50
zhujun98 <zhujun981661@gmail.com>
2019-05-09 20:32:20 +0200

    Unittest: more test for SmartBoundaryLineEdit

--- karaboFAI/gui/tests/test_miscellaneous.py
+++ karaboFAI/gui/tests/test_miscellaneous.py
@@ -1,5 +1,7 @@
 import unittest
 
+from PyQt5.QtTest import QSignalSpy
+
 from karaboFAI.logger import logger
 from karaboFAI.services import FaiServer
 from karaboFAI.gui.misc_widgets import SmartBoundaryLineEdit
@@ -23,11 +25,15 @@ class TestSmartLineEdit(unittest.TestCase):
         widget = SmartBoundaryLineEdit("0, 1")
         self.assertEqual("0, 1", widget._cached)
 
-        # reset a value
+        # set a valid value
+        spy = QSignalSpy(widget.value_changed_sgn)
+        self.assertEqual(0, len(spy))
+
         widget.setText("0, 2")
         widget.returnPressed.emit()
         self.assertEqual("0, 2", widget.text())
         self.assertEqual("0, 2", widget._cached)
+        self.assertEqual(1, len(spy))
 
         # set an invalid value
         with self.assertLogs(logger=logger, level='ERROR'):
@@ -35,3 +41,9 @@ class TestSmartLineEdit(unittest.TestCase):
             widget.returnPressed.emit()
         self.assertEqual("0, 2", widget.text())
         self.assertEqual("0, 2", widget._cached)
+        self.assertEqual(1, len(spy))
+
+        # set a valid value again
+        widget.setText("0, 2")
+        widget.returnPressed.emit()
+        self.assertEqual(2, len(spy))
