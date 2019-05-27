a9a5d2898b222bc5e7b010d0154092f8b30325ab
zhujun <zhujun981661@gmail.com>
2019-05-27 11:23:30 +0200

    Add _w to the file name of Window.
    To distinguish from processor files.

--- karaboFAI/gui/windows/__init__.py
+++ karaboFAI/gui/windows/__init__.py
@@ -1,11 +1,11 @@
-from .correlation import CorrelationWindow
+from .correlation_w import CorrelationWindow
 from .image_tool import ImageToolWindow
-from .overview import OverviewWindow
-from .pulsed_azimuthal_integration import PulsedAzimuthalIntegrationWindow
-from .pump_probe import PumpProbeWindow
-from .roi import RoiWindow
-from .xas import XasWindow
-from .bin import BinWindow
+from .overview_w import OverviewWindow
+from .pulsed_azimuthal_integration_w import PulsedAzimuthalIntegrationWindow
+from .pump_probe_w import PumpProbeWindow
+from .roi_w import RoiWindow
+from .xas_w import XasWindow
+from .bin_w import BinWindow
 
 
 __all__ = [
