afc69ddbc79302e29b640017ad0e3505915f37a2
zhujun98 <zhujun981661@gmail.com>
2019-07-23 11:57:04 +0200

    Fix RoiImageView and add unittest

--- karaboFAI/gui/plot_widgets/tests/test_image_view.py
+++ karaboFAI/gui/plot_widgets/tests/test_image_view.py
@@ -7,7 +7,10 @@ import numpy as np
 from karaboFAI.gui import mkQApp, pyqtgraph
 from karaboFAI.gui.windows.image_tool import _SimpleImageData
 from karaboFAI.gui.plot_widgets.plot_items import ImageItem, MaskItem, RectROI
-from karaboFAI.gui.plot_widgets.image_view import ImageView, ImageAnalysis
+from karaboFAI.gui.plot_widgets.image_view import (
+    ImageView, ImageAnalysis, RoiImageView,
+)
+from karaboFAI.pipeline.data_model import ProcessedData, RoiData
 from karaboFAI.logger import logger
 
 app = mkQApp()
@@ -25,6 +28,27 @@ class TestImageView(unittest.TestCase):
 
         widget = ImageView(has_roi=False)
         self.assertEqual(1, len(widget._plot_widget.plotItem.items))
+
+    def testRoiImageView(self):
+        widget = RoiImageView(1)
+        widget.setImage = MagicMock()
+        processed = ProcessedData(1)
+        processed.image.masked_mean = np.ones((3, 3))
+
+        # invalid ROI rect
+        self.assertListEqual([0, 0, -1, -1], processed.roi.rect1)
+        widget.update(processed)
+        widget.setImage.assert_not_called()
+
+        # invalid ROI rect
+        processed.roi.rect1 = [0, 0, -1, 0]
+        widget.update(processed)
+        widget.setImage.assert_not_called()
+
+        # valid ROI rect
+        processed.roi.rect1 = [0, 0, 2, 2]
+        widget.update(processed)
+        widget.setImage.assert_called_once()
 
 
 class TestImageAnalysis(unittest.TestCase):
@@ -89,3 +113,4 @@ class TestImageAnalysis(unittest.TestCase):
             widget._loadImageMaskImp(fp)
 
         fp.close()
+
