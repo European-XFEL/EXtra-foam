f30d932774a1bb1cb799fadbcc3adff06e164e64
zhujun98 <zhujun981661@gmail.com>
2019-08-11 21:11:28 +0200

    Update CMakeLists.txt for cpp unittest.

--- test/test_xtnumpy.cpp
+++ test/test_xtnumpy.cpp
@@ -1,3 +1,14 @@
+/**
+ * Offline and online data analysis and visualization tool for azimuthal
+ * integration of different data acquired with various detectors at
+ * European XFEL.
+ *
+ * Unittest for xtnumpy.
+ *
+ * Author: Jun Zhu <jun.zhu@xfel.eu>
+ * Copyright (C) European X-Ray Free-Electron Laser Facility GmbH.
+ * All rights reserved.
+ */
 #include <iostream>
 
 #include "gtest/gtest.h"
