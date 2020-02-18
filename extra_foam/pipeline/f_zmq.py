f4c5668ea7c26f7b668ed1cfcbd335f0e8e98b83
zhujun <zhujun981661@gmail.com>
2020-02-18 15:53:25 +0100

    Receive data from multiple bridges.

--- extra_foam/pipeline/f_zmq.py
+++ extra_foam/pipeline/f_zmq.py
@@ -7,6 +7,7 @@ Author: Jun Zhu <jun.zhu@xfel.eu>
 Copyright (C) European X-Ray Free-Electron Laser Facility GmbH.
 All rights reserved.
 """
+from collections import deque
 from threading import Event
 
 import zmq
@@ -18,13 +19,24 @@ from ..utils import run_in_thread
 
 
 class BridgeProxy:
+    """A proxy bridge which can connect to more than one server.
+
+    The current implementation has the following limits:
+    1. All the connections must be alive;
+    2. It is blocked, which means if one connection is fast and the other
+       is slow, the overall performance is limited by the slow one.
+    """
+
+    POLL_TIMEOUT = 100  # timeout of the poller in milliseconds
+
     def __init__(self):
 
         self._context = zmq.Context()
 
         self._client = None
         self._frontend = None
-        self._backend = None
+        self._backend = dict()
+        self._backend_ready = deque()
 
         self._running = False
         self._stopped = Event()
@@ -35,20 +47,23 @@ class BridgeProxy:
         return self._client
 
     def connect(self, endpoints):
+        """Connect the backend to one or more endpoints.
+
+        :param str/list/tuple endpoints: addresses of endpoints.
+        """
         if isinstance(endpoints, str):
             endpoints = [endpoints]
         elif not isinstance(endpoints, (tuple, list)):
             raise ValueError("Endpoints must be either a string or "
                              "a tuple/list of string!")
 
-        self._backend = self._context.socket(zmq.DEALER)
-        self._backend.setsockopt(zmq.LINGER, 0)
         for end in endpoints:
-            self._backend.connect(end)
+            backend = self._context.socket(zmq.DEALER)
+            backend.connect(end)
+            self._backend[end] = backend
 
         frontendpoint = "inproc://frontend"
         self._frontend = self._context.socket(zmq.ROUTER)
-        self._frontend.setsockopt(zmq.LINGER, 0)
         self._frontend.bind(frontendpoint)
 
         self._client = Client(frontendpoint,
@@ -57,41 +72,55 @@ class BridgeProxy:
 
     @run_in_thread()
     def start(self):
+        """Run the proxy in a thread."""
         if self._running:
-            raise RuntimeError("Proxy is already running!")
+            raise RuntimeError(f"{self.__class__} is already running!")
 
         frontend = self._frontend
-        backend = self._backend
 
-        # Initialize poll set
         poller = zmq.Poller()
         poller.register(frontend, zmq.POLLIN)
-        poller.register(backend, zmq.POLLIN)
+        for address, bk in self._backend.items():
+            poller.register(bk, zmq.POLLIN)
+            self._backend_ready.append(address)
 
         self._stopped.clear()
         self._running = True
         while self._running:
-            socks = dict(poller.poll(timeout=100))
+            socks = dict(poller.poll(timeout=self.POLL_TIMEOUT))
 
             if socks.get(frontend) == zmq.POLLIN:
                 message = frontend.recv_multipart()
-                backend.send_multipart(message)
+                if len(self._backend_ready) > 0:
+                    address = self._backend_ready.popleft()
+                    self._backend[address].send_multipart(message)
 
-            if socks.get(backend) == zmq.POLLIN:
-                message = backend.recv_multipart()
-                frontend.send_multipart(message)
+            for address, bk in self._backend.items():
+                if socks.get(bk) == zmq.POLLIN:
+                    message = bk.recv_multipart()
+                    frontend.send_multipart(message)
+                    self._backend_ready.append(address)
+
+        # clean up and close all sockets to avoid problems with buffer
 
         poller.unregister(frontend)
-        poller.unregister(backend)
+        for bk in self._backend.values():
+            poller.unregister(bk)
 
-        # close all sockets to avoid problems with buffer
-        self._backend.close()
+        for bk in self._backend.values():
+            bk.setsockopt(zmq.LINGER, 0)
+            bk.close()
+        self._backend.clear()
+        self._backend_ready.clear()
+        self._frontend.setsockopt(zmq.LINGER, 0)
         self._frontend.close()
         del self._client
+        self._client = None
 
         self._stopped.set()
 
     def stop(self):
+        """Stop the proxy running in a thread."""
         self._running = False
         if not self._stopped.is_set():
             self._stopped.wait()
