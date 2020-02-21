05cc2071da5c1ee39e9876602bd02fa17635070c
zhujun <zhujun981661@gmail.com>
2020-02-21 13:26:46 +0100

    Add unittest

--- extra_foam/pipeline/tests/test_queue.py
+++ extra_foam/pipeline/tests/test_queue.py
@@ -2,14 +2,55 @@ import unittest
 from unittest.mock import patch, PropertyMock
 from queue import Empty, Full
 import random
+from threading import Thread
 
-from extra_foam.pipeline.f_queue import CorrelateQueue
+from extra_foam.pipeline.f_queue import CorrelateQueue, SimpleQueue
 from extra_foam.database import SourceCatalog, SourceItem
 from extra_foam.config import config
 
 
 class TestSimpleQueue(unittest.TestCase):
-    pass
+    def testGeneral(self):
+        queue = SimpleQueue(maxsize=2)
+        self.assertTrue(queue.empty())
+        queue.put_nowait(1)
+        queue.put(2)
+        with self.assertRaises(Full):
+            queue.put(3)
+        self.assertTrue(queue.full())
+        self.assertEqual(2, queue.qsize())
+        self.assertEqual(1, queue.get())
+        self.assertEqual(2, queue.get())
+        with self.assertRaises(Empty):
+            queue.get()
+        self.assertTrue(queue.empty())
+
+        # test no maxsize
+        queue = SimpleQueue()
+        for i in range(100):
+            queue.put(i)
+        self.assertEqual(100, queue.qsize())
+        self.assertFalse(queue.full())
+        queue.clear()
+        self.assertTrue(queue.empty())
+
+    def testMultiThreads(self):
+        def worker1(queue):
+            for i in range(100):
+                queue.put(i)
+
+        def worker2(queue):
+            for _ in range(100):
+                queue.get()
+
+        queue = SimpleQueue()
+        t1 = Thread(target=worker1, args=(queue,))
+        t2 = Thread(target=worker2, args=(queue,))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        self.assertTrue(queue.empty())
 
 
 @patch.dict(config._data, {"DETECTOR": "ABC"})
---
+++ extra_foam/pipeline/tests/test_worker.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import multiprocessing as mp
+
+from extra_foam.pipeline.exceptions import ProcessingError, StopPipelineError
+from extra_foam.pipeline.worker import TrainWorker, PulseWorker
+from extra_foam.config import config
+
+
+@patch.dict(config._data, {"DETECTOR": "LPD"})
+class TestWorker(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._pause_ev = mp.Event()
+        cls._close_ev = mp.Event()
+
+    @patch('extra_foam.ipc.ProcessLogger.debug')
+    @patch('extra_foam.ipc.ProcessLogger.error')
+    def testRunTasks(self, error, debug):
+        for kls in (TrainWorker, PulseWorker):
+            worker = kls(self._pause_ev, self._close_ev)
+            for proc in worker._tasks:
+                proc.update = MagicMock()
+                proc.process = MagicMock()
+            worker._run_tasks({})
+
+            proc = worker._tasks[0]
+
+            # test responses to different Exceptions
+
+            proc.process.side_effect = ValueError()
+            worker._run_tasks({})
+            debug.assert_called_once()
+            self.assertIn("Unexpected Exception", debug.call_args_list[0][0][0])
+            debug.reset_mock()
+            error.assert_called_once()
+            error.reset_mock()
+
+            proc.process.side_effect = ProcessingError()
+            worker._run_tasks({})
+            debug.assert_called_once()
+            self.assertNotIn("Unexpected Exception", debug.call_args_list[0][0][0])
+            debug.reset_mock()
+            error.assert_called_once()
+            error.reset_mock()
+
+            proc.process.side_effect = StopPipelineError()
+            with self.assertRaises(StopPipelineError):
+                worker._run_tasks({})
+            debug.reset_mock()
+            error.reset_mock()
