46359474290de547fab98394efad4d334ace60e9
zhujun98 <zhujun981661@gmail.com>
2020-05-25 17:13:39 +0200

    Simplify binding code

--- src/extra_foam/f_geometry.cpp
+++ src/extra_foam/f_geometry.cpp
@@ -41,67 +41,36 @@ void declareGeometry(py::module &m, const std::string& detector_name)
     .def_readonly_static("asic_shape", &Detector::asic_shape)
     .def_readonly_static("asic_grid_shape", &Detector::asic_grid_shape);
 
-#define FOAM_POSITION_ALL_MODULES_SINGLE_IMP(SRC_TYPE, DST_TYPE)                                      \
-  cls.def("positionAllModules",                                                                       \
-    (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 3>&, xt::pytensor<DST_TYPE, 2>&, bool) const)    \
-    &Geometry::positionAllModules,                                                                    \
-    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);
-
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(float, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(uint16_t, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(int16_t, float) // for ePix100
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(bool, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(uint16_t, uint16_t)
-  FOAM_POSITION_ALL_MODULES_SINGLE_IMP(bool, bool)
-
-#define FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(SRC_TYPE, DST_TYPE)                                          \
+#define FOAM_POSITION_ALL_MODULES(SRC_TYPE, DST_TYPE)                                                            \
+  cls.def("positionAllModules",                                                                                  \
+    (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 3>&, xt::pytensor<DST_TYPE, 2>&, bool) const)               \
+    &Geometry::positionAllModules,                                                                               \
+    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);                \
   cls.def("positionAllModules",                                                                                  \
     (void (Geometry::*)(const std::vector<xt::pytensor<SRC_TYPE, 2>>&, xt::pytensor<DST_TYPE, 2>&, bool) const)  \
     &Geometry::positionAllModules,                                                                               \
-    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);
-
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(float, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(uint16_t, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(int16_t, float) // for ePix100
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(bool, float)
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(uint16_t, uint16_t)
-  FOAM_POSITION_ALL_MODULES_SINGLE_VECTOR_IMP(bool, bool)
-
-#define FOAM_POSITION_ALL_MODULES_IMP(SRC_TYPE, DST_TYPE)                                                   \
-  cls.def("positionAllModules",                                                                             \
-    (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 4>&, xt::pytensor<DST_TYPE, 3>&, bool) const)          \
-    &Geometry::positionAllModules,                                                                          \
-    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);
-
-  FOAM_POSITION_ALL_MODULES_IMP(float, float)
-  FOAM_POSITION_ALL_MODULES_IMP(uint16_t, float)
-  FOAM_POSITION_ALL_MODULES_IMP(bool, float)
-  FOAM_POSITION_ALL_MODULES_IMP(uint16_t, uint16_t)
-  FOAM_POSITION_ALL_MODULES_IMP(bool, bool)
-
-#define FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(SRC_TYPE, DST_TYPE)                                             \
+    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);                \
+  cls.def("positionAllModules",                                                                                  \
+    (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 4>&, xt::pytensor<DST_TYPE, 3>&, bool) const)               \
+    &Geometry::positionAllModules,                                                                               \
+    py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);                \
   cls.def("positionAllModules",                                                                                  \
     (void (Geometry::*)(const std::vector<xt::pytensor<SRC_TYPE, 3>>&, xt::pytensor<DST_TYPE, 3>&, bool) const)  \
     &Geometry::positionAllModules,                                                                               \
     py::arg("src").noconvert(), py::arg("dst").noconvert(), py::arg("ignore_asic_edge") = false);
 
-  FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(float, float)
-  FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(uint16_t, float)
-  FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(bool, float)
-  FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(uint16_t, uint16_t)
-  FOAM_POSITION_ALL_MODULES_VECTOR_SRC_IMP(bool, bool)
+  FOAM_POSITION_ALL_MODULES(float, float)
+  FOAM_POSITION_ALL_MODULES(uint16_t, float)
+  FOAM_POSITION_ALL_MODULES(int16_t, float) // for ePix100
+  FOAM_POSITION_ALL_MODULES(bool, float)
+  FOAM_POSITION_ALL_MODULES(uint16_t, uint16_t)
+  FOAM_POSITION_ALL_MODULES(bool, bool)
 
-#define FOAM_DISMANTLE_ALL_MODULES_SINGLE_IMP(SRC_TYPE, DST_TYPE)                                  \
+#define FOAM_DISMANTLE_ALL_MODULES(SRC_TYPE, DST_TYPE)                                             \
   cls.def("dismantleAllModules",                                                                   \
   (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 2>&, xt::pytensor<DST_TYPE, 3>&) const)         \
     &Geometry::dismantleAllModules,                                                                \
-    py::arg("src").noconvert(), py::arg("dst").noconvert());
-
-  FOAM_DISMANTLE_ALL_MODULES_SINGLE_IMP(float, float)
-  FOAM_DISMANTLE_ALL_MODULES_SINGLE_IMP(uint16_t, uint16_t)
-  FOAM_DISMANTLE_ALL_MODULES_SINGLE_IMP(bool, bool)
-
-#define FOAM_DISMANTLE_ALL_MODULES(SRC_TYPE, DST_TYPE)                                             \
+    py::arg("src").noconvert(), py::arg("dst").noconvert());                                       \
   cls.def("dismantleAllModules",                                                                   \
   (void (Geometry::*)(const xt::pytensor<SRC_TYPE, 3>&, xt::pytensor<DST_TYPE, 4>&) const)         \
     &Geometry::dismantleAllModules,                                                                \
