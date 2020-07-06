1c37b77afbfcc0a31be586fb1d6b7fc1e8ea9e34
zhujun98 <zhujun981661@gmail.com>
2020-07-06 18:54:51 +0200

    Update pyqtgraph
    https://github.com/pyqtgraph/pyqtgraph/pull/1296
    https://github.com/pyqtgraph/pyqtgraph/pull/1295
    https://github.com/pyqtgraph/pyqtgraph/pull/1284
    https://github.com/pyqtgraph/pyqtgraph/pull/1285

--- extra_foam/gui/pyqtgraph/graphicsItems/TextItem.py
+++ extra_foam/gui/pyqtgraph/graphicsItems/TextItem.py
@@ -67,27 +67,34 @@ class TextItem(GraphicsObject):
         """
         if color is not None:
             self.setColor(color)
-        self.textItem.setPlainText(text)
-        self.updateTextPos()
-        
-    def setPlainText(self, *args):
+        self.setPlainText(text)
+
+    def setPlainText(self, text):
         """
         Set the plain text to be rendered by this item. 
         
         See QtGui.QGraphicsTextItem.setPlainText().
         """
-        self.textItem.setPlainText(*args)
-        self.updateTextPos()
+        if text != self.toPlainText():
+            self.textItem.setPlainText(text)
+            self.updateTextPos()
+
+    def toPlainText(self):
+        return self.textItem.toPlainText()
         
-    def setHtml(self, *args):
+    def setHtml(self, html):
         """
         Set the HTML code to be rendered by this item. 
         
         See QtGui.QGraphicsTextItem.setHtml().
         """
-        self.textItem.setHtml(*args)
-        self.updateTextPos()
+        if self.toHtml() != html:
+            self.textItem.setHtml(html)
+            self.updateTextPos()
         
+    def toHtml(self):
+        return self.textItem.toHtml()
+
     def setTextWidth(self, *args):
         """
         Set the width of the text.
@@ -141,6 +148,13 @@ class TextItem(GraphicsObject):
         offset = (br - tl) * self.anchor
         self.textItem.setPos(-offset)
         
+        ### Needed to maintain font size when rendering to image with increased resolution
+        #self.textItem.resetTransform()
+        ##self.textItem.rotate(self.angle)
+        #if self._exportOpts is not False and 'resolutionScale' in self._exportOpts:
+            #s = self._exportOpts['resolutionScale']
+            #self.textItem.scale(s, s)
+
     def boundingRect(self):
         return self.textItem.mapToParent(self.textItem.boundingRect()).boundingRect()
 
@@ -169,7 +183,15 @@ class TextItem(GraphicsObject):
             p.setRenderHint(p.Antialiasing, True)
             p.drawPolygon(self.textItem.mapToParent(self.textItem.boundingRect()))
         
+    def setVisible(self, v):
+        GraphicsObject.setVisible(self, v)
+        if v:
+            self.updateTransform()
+
     def updateTransform(self, force=False):
+        if not self.isVisible():
+            return
+
         # update transform such that this item has the correct orientation
         # and scaling relative to the scene, but inherits its position from its
         # parent.
