7ef53f6e79a576ed095de899e98daf659f1bf9d2
Thomas Kluyver <thomas.kluyver@xfel.eu>
2024-08-13 16:43:30 +0100

    wget doesn't like downloading to path containing ..

--- thirdparty/scripts/build_redis.sh
+++ thirdparty/scripts/build_redis.sh
@@ -13,7 +13,7 @@ REDIS_BUILD_DIR=${TP_DIR}/pkg/redis/
 if [[ ! -f ${REDIS_BUILD_DIR}/src/redis-server ]]; then
 
     REDIS_DOWNLOAD=${REDIS_VERSION}.tar.gz
-    wget https://github.com/antirez/redis/archive/${REDIS_DOWNLOAD} -P ${TP_DIR}
+    wget https://github.com/antirez/redis/archive/${REDIS_DOWNLOAD} -P "$(realpath ${TP_DIR})"
 
     mkdir -p ${REDIS_BUILD_DIR}
     tar -xzf ${TP_DIR}/${REDIS_DOWNLOAD} --strip-components=1 -C ${REDIS_BUILD_DIR}
